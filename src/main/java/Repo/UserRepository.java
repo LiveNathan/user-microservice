80c783fba408836260c3c413ab322ea31a458af2
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-03-02 20:47:25 -0600

    user micro

---
+++ src/main/java/Repo/UserRepository.java
@@ -0,0 +1,8 @@
+package Repo;
+
+import Model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
