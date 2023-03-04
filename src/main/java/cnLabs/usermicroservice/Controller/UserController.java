bb7051513f977118c64d8b533ab22a095f1394a7
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2023-03-03 18:27:31 -0600

    connected to Eureka

--- src/main/java/cnLabs/usermicroservice/Controller/UserController.java
+++ src/main/java/cnLabs/usermicroservice/Controller/UserController.java
@@ -2,11 +2,17 @@ package cnLabs.usermicroservice.Controller;
 
 import cnLabs.usermicroservice.Model.User;
 import cnLabs.usermicroservice.Service.UserService;
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -62,5 +68,10 @@ public class UserController {
         } catch (Exception e) {
             return ResponseEntity.status(500).body(e.getMessage());
         }
+    }
+
+    @PostMapping("/events/{id}")
+    public ResponseEntity<?> userCartEvent(@PathVariable Long id) {
+        return ResponseEntity.ok("ok: " + id);
     }
 }
